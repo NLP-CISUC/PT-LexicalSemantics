25a2fa74a5f0fac1a2dd9e784a8236ac8a5013ff
Alexandre Rademaker <arademaker@gmail.com>
2020-10-04 12:21:31 -0300

    breaking relations by pairs POS

--- OWN-PT/process.lisp
+++ OWN-PT/process.lisp
@@ -1,27 +1,30 @@
 
 (ql:quickload '(:cl-ppcre :yason))
 
-(defun filename (url)
+(defun localname (url)
   (multiple-value-bind (a b)
       (cl-ppcre:scan-to-strings "<https://w3id.org/own-pt/wn30/schema/([a-zA-Z]+)>" url)
     (declare (ignore a))
-    (format nil "~a.txt" (aref b 0))))
+    (aref b 0)))
+
+(defun filename (type1 rel type2)
+  (format nil "~a-~a-~a.txt" (localname rel) (localname type1) (localname type2)))
 
 (defun collect-wn (jfile stats)
   (let ((tb (make-hash-table :test #'equal)))
     (dolist (res (gethash "values" (yason:parse jfile)) tb)
-      (destructuring-bind (a b c)
+      (destructuring-bind (p t1 rel t2 rs)
 	  res
-	(let* ((target (string-downcase (string-trim '(#\" #\_ #\Space) a)))
-	       (rel    (filename b))
-	       (tmp-c  (mapcar #'string-downcase (cl-ppcre:split "/" (string-trim '(#\") c))))
+	(let* ((target (string-downcase (string-trim '(#\" #\_ #\Space) p)))
+	       (fn     (filename t1 rel t2))
+	       (tmp    (mapcar #'string-downcase (cl-ppcre:split "/" (string-trim '(#\") rs))))
 	       (words  (remove-if-not (lambda (w) (> (gethash w stats 0) 1))
 				      (mapcar (lambda (w) (string-trim '(#\_ #\Space) w))
-					      (remove target (remove-duplicates tmp-c :test #'equal)
+					      (remove target (remove-duplicates tmp :test #'equal)
 						      :test #'equal)))))
 	  (if words
 	      (push (cons target (format nil "~{~a~^/~}" words))
-		    (gethash rel tb))))))))
+		    (gethash fn tb))))))))
 
 (defun collect-stats (fn)
   (let ((tb (make-hash-table :test #'equal)))
